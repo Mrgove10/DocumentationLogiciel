9ee611b7b98a92e8ff9f13ced431a319a531cf31
Adrien RICHARD <richardadrien0@gmail.com>
2021-04-19 15:05:12 +0200

    added razor file support
    doxygen recursive
    
    Comments for CI
    
    doxygen recursive
    
    doxygen recursive
    
    doxygen recursive

--- DocumentationLogicielle.App/DocumentationLogicielle.Models/User.cs
+++ DocumentationLogicielle.App/DocumentationLogicielle.Models/User.cs
@@ -4,7 +4,14 @@ namespace DocumentationLogicielle.Models
 {
     public class User
     {
+        /// <summary>
+        /// Ceci est le login de l'utilisateur
+        /// </summary>
         public string Login { get; set; }
+
+        /// <summary>
+        /// Voici son mot de passe
+        /// </summary>
         public string Password { get; set; }
     }
 }

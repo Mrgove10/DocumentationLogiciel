0c9de6d8d48f654bf25e4764ccb40239fa17d953
Lou BEGE <wolfla@hotmail.fr>
2021-05-13 19:14:33 +0200

    Feat: Add notnull for db + property available until

--- DocumentationLogicielle/DocumentationLogicielle.Models/Material.cs
+++ DocumentationLogicielle/DocumentationLogicielle.Models/Material.cs
@@ -9,20 +9,23 @@ namespace DocumentationLogicielle.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        
+
         /// <summary>
         /// Label assigned to the material
         /// </summary>
+        [NotNull]
         public string Label { get; set; }
 
         /// <summary>
         /// Unitary price 
         /// </summary>
+        [NotNull]
         public float Price { get; set; }
 
         /// <summary>
         /// Quantity available  
         /// </summary>
+        [NotNull]
         public int Quantity { get; set; }
     }
 }
--- DocumentationLogicielle/DocumentationLogicielle.Models/Product.cs
+++ DocumentationLogicielle/DocumentationLogicielle.Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using SQLite;
 
 namespace DocumentationLogicielle.Models
@@ -8,22 +8,34 @@ namespace DocumentationLogicielle.Models
     /// </summary>
     public class Product
     {
+        /// <summary>
+        /// Id of the product
+        /// </summary>
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         
         /// <summary>
         /// Label of the product
         /// </summary>
+        [NotNull]
         public string Label { get; set; }
 
         /// <summary>
         /// Price of the product
         /// </summary>
+        [NotNull]
         public float Price { get; set; }
-        
+
         /// <summary>
-        /// Quantitiy of the product
+        /// Quantity of the product
         /// </summary>
+        [NotNull]
         public int Quantity { get; set; }
+
+        /// <summary>
+        /// Product available until a date
+        /// </summary>
+        [NotNull]
+        public DateTime AvailableUntil { get; set; }
     }
 }
